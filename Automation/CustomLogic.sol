5c25dea86e96e62b54aecc5e851e74d286bac6bf
Copernicium282 <amitrmblr@gmail.com>
2026-04-15 03:15:36 +0530

    Day 2 of ChainLink Fundamentals

---
+++ Automation/CustomLogic.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT  
+pragma solidity ^0.8.30;
+
+import { AutomationCompatibleInterface } from "@chainlink/contracts@1.3.0/src/v0.8/automation/AutomationCompatible.sol";
+
+contract CustomLogic is AutomationCompatibleInterface {
+
+    uint256 public counter;  
+    uint256 public immutable i_updateInterval;  
+    uint256 public lastTimeStamp;
+
+    constructor(uint256 _updateInterval) {  
+        i_updateInterval = _updateInterval;  
+        lastTimeStamp = block.timestamp;
+    }
+
+    function checkUpkeep(  
+        bytes calldata /* checkData */  
+    )  
+        external  
+        view  
+        override  
+        returns (bool upkeepNeeded, bytes memory performData)  
+    {  
+        // Checks if the time elapsed between last upkeep and now is greater than the update interval.
+        upkeepNeeded = (block.timestamp - lastTimeStamp) > i_updateInterval;
+        performData = "";
+    }
+
+    function performUpkeep(bytes calldata) external override {  
+        if ((block.timestamp - lastTimeStamp) > i_updateInterval) {  
+            lastTimeStamp = block.timestamp;  
+            counter = counter + 1;  
+        }  
+    }  
+}
